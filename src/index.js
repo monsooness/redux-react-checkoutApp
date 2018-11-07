d9383bfc61d38bd625794a4076732fe940ac9a4f
Monsoon <nepzsoon@gmail.com>
2018-11-06 19:06:25 -0800

    Implement provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
